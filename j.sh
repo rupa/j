b57503abbffcfcda0b72c973683e7fd4ef34e3c5
rupa <rupa@lrrr.us>
2009-03-06 01:53:57 -0500

    major refactoring. add --s - shortest match

--- j.sh
+++ j.sh
@@ -8,18 +8,18 @@
 #   cd around for a while
 #
 # USE:
-#   j [--h[elp]] [--r] [--l] [regex1 ... regexn]
+#   j [--h[elp]] [--l] [--r] [--s] [regex1 ... regexn]
 #                       with no args, returns full list (same as j --l)
 #     regex1 ... regexn jump to the most used directory matching all masks
 #     --l               show the list instead of jumping
-#     --r               order by recently used instead of most used. (If this
-#                       option is used, it must be the first option.)
+#     --r               order by recently used instead of most used.
+#     --s               shortest matching path
 #
 # TIPS:
 #   Some handy aliases:
 #     alias jl='j --l'
 #     alias jr='j --r'
-#     alias jrl='j --r --l'
+#     alias js='j --s'
 #
 # CREDITS:
 #   Joel Schaerer aka joelthelion for autojump
@@ -27,10 +27,10 @@
 j() {
  # change jfile if you already have a .j file for something else
  local jfile=$HOME/.j
- if [ "$1" = "--add" ]; then
-  shift
+ [ "$1" = "--add" ] && {
   # we're in $HOME all the time, let something else get all the good letters
   [ "$*" = "$HOME" ] && return
+  shift
   awk -v q="$*" -v t="$(date +%s)" -F"|" '
    $2 >= 1 { 
     if( $1 == q ) {
@@ -51,63 +51,75 @@ j() {
    }
   ' $jfile 2>/dev/null > $jfile.tmp
   mv -f $jfile.tmp $jfile
- elif [ "$1" = "--h" -o "$1" = "--help" ]; then
-  echo "j [--h[elp]] [--r] [--l] [regex1 ... regexn]"
- elif [ "$1" = "" -o "$1$2" = "--r" -o "$1" = "--l" -o "$1$2" = "--r--l" ];then
-  [ "$1" = "--r" ] && local r=r && shift
-  [ "$1" = "--l" ] && shift
-  awk -v q="$*" -v t="$(date +%s)" -v r="$r" -F"|" '
+  return
+ }
+ # tab completion
+ [ "$1" = "--complete" ] && {
+  awk -v q="$2" -F"|" '
+   BEGIN { split(substr(q,3),a," ") }
+   { 
+    if( system("test -d \"" $1 "\"") ) next
+    for( i in a ) $1 !~ a[i] && $1 = ""; if( $1 ) print $1
+   }
+  ' $jfile 2>/dev/null
+  return
+ }
+ local x; local out
+ for x do case $x in
+  --h*) echo "j [--h[elp]] [--r] [--l] [regex1 ... regexn]"; return;;
+  --l)local list=1;;
+  --r)local recent=r;;
+  --s)local short=1;;
+    *)local out="$out $x";;
+ esac; shift; done
+ set -- $out
+ if [ ! $1 -o "$list" ]; then
+  [ "$short" ] && return
+  awk -v q="$*" -v t="$(date +%s)" -v r="$recent" -F"|" '
    BEGIN { f = 2; split(q,a," "); if( r ) f = 3 }
    {
     if( system("test -d \"" $1 "\"") ) next
     for( i in a ) $1 !~ a[i] && $1 = ""
     if( $1 ) if( f == 3 ) { print t - $f "\t" $1 } else print $f "\t" $1
    }
-  ' $jfile 2>/dev/null | sort -n$r
- # for completion
- elif [ "$1" = "--complete" -o "$2" = "--complete" ];then
-  awk -v q="$2" -F"|" '
-   BEGIN {
-    if( substr(q,1,5) == "j --r" ) {
-     split(substr(q,7),a," ")
-    } else split(substr(q,3),a," ")
-   }
-   { 
-    if( system("test -d \"" $1 "\"") ) next
-    for( i in a ) $1 !~ a[i] && $1 = ""; if( $1 ) print $1
-   }
-  ' $jfile 2>/dev/null
+  ' $jfile 2>/dev/null | sort -n$recent
+ # if we hit enter on a completion just go there
+ elif [ -d "/${out#*/}" ]; then
+  cd "/${out#*/}"
+ # prefer case sensitive
  else
-  # if we hit enter on a completion just go there (ugh, this is ugly)
-  local x=$*; x=/${x#*/}
-  [ -d "$x" ] && cd "$x" && return
-  # prefer case sensitive
-  local cd=$(awk -v q="$*" -F"|" '
-   BEGIN { 
-    if( substr(q,1,3) == "--r" ) {
-     split(substr(q,5),a," ")
-     f = 3
-    } else {
-     split(q,a," ")
-     f = 2
-    }
-   }
+  out=$(awk -v q="$*" -v s="$short" -v r="$recent" -F"|" '
+   BEGIN { split(q,a," "); if( r ) { f = 3 } else f = 2 }
    { 
     if( system("test -d \"" $1 "\"") ) next
     for( i in a ) $1 !~ a[i] && $1 = ""
-    if( $1 ) { print $f "\t" $1; x = 1 }
-   }
-   END {
-    if( x ) exit
-    close(FILENAME)
-    while( getline < FILENAME ) {
-     if( system("test -d \"" $1 "\"") ) continue
-     for( i in a ) tolower($1) !~ tolower(a[i]) && $1 = ""
-     if( $1 ) print $f "\t" $1
+    if( $1 ) {
+     if( s ) {
+      if( length($1) <= length(x) ) {
+       x = $1
+      } else if( ! x ) x = $1
+     } else if( $f >= dx ) { x = $1; dx = $f }
     }
    }
-  ' $jfile 2>/dev/null | sort -nr | head -n 1 | cut -f 2)
-  [ "$cd" ] && cd "$cd"
+   END {
+    if( ! x ) {
+     close(FILENAME)
+     while( getline < FILENAME ) {
+      if( system("test -d \"" $1 "\"") ) continue
+      for( i in a ) tolower($1) !~ tolower(a[i]) && $1 = ""
+      if( $1 ) {
+       if( s ) {
+        if( length($1) <= length(x) ) {
+         x = $1
+        } else if( ! x ) x = $1
+       } else if( $f >= dx ) { x = $1; dx = $f }
+      }
+     }
+    }
+    if( x ) print x
+   }
+  ' $jfile)
+  [ "$out" ] && cd "$out"
  fi
 }
 # tab completion for j
